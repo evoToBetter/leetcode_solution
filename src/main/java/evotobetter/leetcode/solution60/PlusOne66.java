0cce8b603808ec6ec5e1df557df35e69559e346e
evoToBetter <stonelee1637@163.com>
2020-07-03 13:49:37 +0800

    add solution for problem 66

---
+++ src/main/java/evotobetter/leetcode/solution60/PlusOne66.java
@@ -0,0 +1,5 @@
+package evotobetter.leetcode.solution60;
+
+public interface PlusOne66 {
+    int[] plusOne(int[] digits);
+}
---
+++ src/test/java/evotobetter/leetcode/solution60/TestPlusOne66.java
@@ -0,0 +1,37 @@
+package evotobetter.leetcode.solution60;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class TestPlusOne66 {
+    private PlusOne66 func;
+
+    @BeforeClass
+    public void setup(){
+        func=new PlusOne66_1();
+    }
+
+    @Test
+    public void testAllNine(){
+        int[] digits=new int[5];
+        Arrays.setAll(digits,i->9);
+        int[] result=func.plusOne(digits);
+        Assert.assertEquals(result.length,digits.length+1);
+        Assert.assertEquals(result[0],1);
+        for(int i=1; i<result.length;i++){
+            Assert.assertEquals(result[i],0);
+        }
+    }
+
+    @Test
+    public void testEndWithNine(){
+        int[] digits={1,9,9};
+        int[] result=func.plusOne(digits);
+        Assert.assertEquals(result[0],2);
+        Assert.assertEquals(result[1],0);
+        Assert.assertEquals(result[2],0);
+    }
+}
