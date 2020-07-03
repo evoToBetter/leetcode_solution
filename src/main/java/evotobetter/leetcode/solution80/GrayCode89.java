56f389ea5c849e14e3fff60f933d0e0f437b5c7c
evoToBetter <stonelee1637@163.com>
2020-07-03 11:48:20 +0800

    solution for problem 89

---
+++ src/main/java/evotobetter/leetcode/solution80/GrayCode89.java
@@ -0,0 +1,7 @@
+package evotobetter.leetcode.solution80;
+
+import java.util.List;
+
+public interface GrayCode89 {
+    List<Integer> grayCode(int n);
+}
---
+++ src/test/java/evotobetter/leetcode/solution80/TestGrayCode.java
@@ -0,0 +1,45 @@
+package evotobetter.leetcode.solution80;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestGrayCode {
+    private GrayCode89 func;
+
+    @BeforeClass
+    public void setup(){
+        func=new GrayCode89_3();
+    }
+
+    @Test
+    public void testGrayCode(){
+        int n=3;
+        List<Integer> result =func.grayCode(n);
+        Assert.assertEquals(result.size(),(int)Math.pow(2,n));
+        for(int i=1; i<result.size();i++){
+            if(!isPower2(Math.abs(result.get(i)-result.get(i-1)))){
+                System.out.println(result.get(i)+" and "+result.get(i-1)+" not meet the requirement of gray code");
+                Assert.fail();
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(Integer.toBinaryString(-5));
+        System.out.println(Integer.toBinaryString(5));
+        System.out.println(5&-5);
+        System.out.println((5&-5)<<1);
+        System.out.println(5^((5&-5)<<1));
+    }
+
+    private boolean isPower2(int n){
+        if(n<1){
+            return false;
+        }
+        return (n&(n-1))==0;
+    }
+}
